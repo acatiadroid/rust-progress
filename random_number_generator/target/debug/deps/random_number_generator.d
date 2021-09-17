615d74699bea3f45c0a24a6f5e95b433674f9c79
Luke <69216256+acatiadroid@users.noreply.github.com>
2021-09-17 23:05:15 +0100

    Add random number generator

---
+++ random_number_generator/target/debug/deps/random_number_generator.d
@@ -0,0 +1,5 @@
+C:\Users\quzon\OneDrive\Documents\Rust\random_number_generator\target\debug\deps\random_number_generator.exe: src\main.rs
+
+C:\Users\quzon\OneDrive\Documents\Rust\random_number_generator\target\debug\deps\random_number_generator.d: src\main.rs
+
+src\main.rs:
---
+++ random_number_generator/target/debug/random_number_generator.d
@@ -0,0 +1,1 @@
+C:\Users\quzon\OneDrive\Documents\Rust\random_number_generator\target\debug\random_number_generator.exe: C:\Users\quzon\OneDrive\Documents\Rust\random_number_generator\src\main.rs
