70c304ec93f101799ccac3629d39aa1e7e3157c7
Luke <69216256+acatiadroid@users.noreply.github.com>
2021-09-17 23:06:36 +0100

    Remove unneeded prints

--- random_number_generator/src/main.rs
+++ random_number_generator/src/main.rs
@@ -1,20 +1,7 @@
-use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    let random_number = rand::thread_rng().gen_range(1..101);
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number);
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+    println!("The random number is: {}", random_number);
 }
