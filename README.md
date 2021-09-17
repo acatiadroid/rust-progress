59beb0ff34a7472865326140bdfd52296b4f33be
Luke <69216256+acatiadroid@users.noreply.github.com>
2021-09-17 22:43:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rust progress
+I am in the process of learning the Rust programming language.
+
+This repository is to "track" my progress whilst following the documentation.
---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\quzon\OneDrive\Documents\Rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\quzon\OneDrive\Documents\Rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
